c3c7419f404cc4e18bdf17be5c3f127f47d4d4a3
Kashish <kashish@ideaindia.co.in>
2026-07-08 13:54:08 +0530

    changes in config file

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,55 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProd ? "/allsoll" : "",
+  assetPrefix: isProd ? "/allsoll/" : "",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'allsoll.com',
+        protocol: "https",
+        hostname: "allsoll.com",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'assets.mixkit.co',
+        protocol: "https",
+        hostname: "assets.mixkit.co",
       },
     ],
-    unoptimized: true,
   },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'allsoll.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.mixkit.co',
+//       },
+//     ],
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
